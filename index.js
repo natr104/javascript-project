68015cfbd83715a0300ced98e3fb9ae13a5ed66e
Nathan <troy.nathan@gmail.com>
2022-03-09 17:29:20 +1100

    implemented villager cards

--- index.js
+++ index.js
@@ -1,24 +1,51 @@
-//const result = fetch('https://acnhapi.com/v1/villagers')
-
-// result
-// .then(response => response.json())
-// .then(data => {
-//     console.log(data)
-    
-// })
+const villagerListSection = document.querySelector('section#villager-list')
 const defaultURL = 'https://acnhapi.com/v1/villagers'
+let villagerQuantity = 10
 
-async function fetchVillagerDetails(url) {   
+const fetchVillagers = async () => {
+    for(let i=1; i <= villagerQuantity; i++) {
+        await getVillager(i)
+    }
+}
+
+const getVillager = async id => {   
     try {
-        const response = await fetch(url)
-        const data = await response.json()
-        return data
+        const response = await fetch(`https://acnhapi.com/v1/villagers/${id}`)
+        const villager = await response.json()
+        console.log(villager)
+        createVillagerCard(villager)
     } catch (error) {
         console.error(error)
     }
 }
 
-function renderVillagerCards(data) {
-    const section = document.querySelector('#villager-list')
-    
+fetchVillagers()
+
+function createVillagerCard(villager) {
+    let html = ""
+    html += renderVillagerCard(villager)
+    villagerListSection.innerHTML += html
 }
+
+function renderVillagerCard({name, icon_uri}) {
+    const villagerName = name['name-USen']
+    
+    return `
+        <div
+            class="villager-card"
+            villager-name='${villagerName}'
+        >
+            <h2
+                class="villager-name"
+                villager-name='${villagerName}'
+            >
+                ${villagerName}
+            </h2>
+            <img
+                src='${icon_uri}'
+                villager-name='${villagerName}'
+            />
+        </div>
+    `    
+}
+
